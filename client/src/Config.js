f15cc123ed841d23a80d106a369228ac4e884b4d
kimgahyeon <kimgostring@naver.com>
2021-05-25 01:11:01 +0900

    무비앱 시리즈 #4 Landing Page 만들기 (1)

--- client/src/Config.js
+++
@@ -1,3 +0,0 @@
-export const USER_SERVER = '/api/users';
-export const API_URL = 'https://api.themoviedb.org/3';
-export const IMG_URL = 'https://image.tmdb.org/t/p';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import { withRouter } from 'react-router-dom';
-import { USER_SERVER } from '../../../Config';
+import { USER_SERVER, API_URL, API_KEY, IMG_URL } from '../../../Config';
+import MainImg from './Section/MainImg';
 
 function LandingPage(props) {
     const onClickHandler = () => {
@@ -14,18 +15,54 @@ function LandingPage(props) {
                 }
             });
     };
+
+    const [Movies, setMovies] = useState([]);
+    const [MainMovie, setMainMovie] = useState();
+
+    // API에서 정보 가져오기, 현재 인기있는 영화 정보
+    useEffect(() => {
+        // url 값 이용
+        const endpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        fetch(endpoint) // fetch와 axios는 유사한 함수
+        .then(res => res.json())
+        .then(res => {
+            setMovies([res.results]);
+            setMainMovie(res.results[0]); // 맨 처음 정보
+        });
+    }, []);
     
     return (
         <div style={{
-            display: 'flex', justifyContent: 'center', alignItems: 'center', 
-            width: '100%', height: '100vh'
+            width: '100%', margin: 0
         }}>
-            <h2>시작 페이지</h2>
 
-            <button onClick={onClickHandler}>
-                로그아웃
-            </button>
-        </div>
+            { /* main img 컴포넌트 추가, MainMovie 정보를 가져온 이후 렌더링해야 함 */ 
+                MainMovie && 
+                <MainImg 
+                    img={`${IMG_URL}/w1280/${MainMovie.backdrop_path}`} 
+                    title={MainMovie.original_title}
+                    text={MainMovie.overview}
+                />
+            }
+            
+            <div style={{
+                width: '85%', margin: '1rem auto'
+            }}>
+                <h2>Movie by latest</h2>
+                <button onClick={onClickHandler}>
+                    Log out
+                </button>
+                <hr /> { /* 선 긋기 */ }
+                
+                { /* movie grid cards */ }
+            </div>
+
+            <div style={{
+                display: 'flex', justifyContent: 'center'
+            }}>
+                <button>Load More</button>
+            </div>
+        </div>  
     );
 }
 
